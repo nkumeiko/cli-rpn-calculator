74069339ea8e575481bd15e0d06d9e9d0ad7018f
Nataliia Kumeiko <nkumeiko@gmail.com>
2019-01-14 15:04:24 +0200

    Add test cases from examples

--- spec/app/app_spec.rb
+++ spec/app/app_spec.rb
@@ -5,12 +5,11 @@ RSpec.describe App do
 
   before do
     stub_const 'STDIN', double()
+    allow(STDOUT).to receive(:print).with('> ')
   end
 
   context '#run' do
     it 'processes multiple commands and quits' do
-      allow(STDOUT).to receive(:print).with('> ')
-
       expect(STDIN).to receive(:gets).and_return('4 +')
       expect(STDOUT).to receive(:puts).with('Not enough operands.')
 
@@ -22,6 +21,71 @@ RSpec.describe App do
 
       expect(STDIN).to receive(:gets).and_return('/')
       expect(STDOUT).to receive(:puts).with(1.0)
+
+      expect(STDIN).to receive(:gets).and_return('q')
+      subject.run
+    end
+
+    it 'processes test case #1 and quits' do
+      expect(STDIN).to receive(:gets).and_return('5')
+      expect(STDOUT).to receive(:puts).with(5.0)
+
+      expect(STDIN).to receive(:gets).and_return('8')
+      expect(STDOUT).to receive(:puts).with(8.0)
+
+      expect(STDIN).to receive(:gets).and_return('+')
+      expect(STDOUT).to receive(:puts).with(13.0)
+
+      expect(STDIN).to receive(:gets).and_return('q')
+      subject.run
+    end
+
+    it 'processes test case #2 and quits' do
+      expect(STDIN).to receive(:gets).and_return('5 8 +')
+      expect(STDOUT).to receive(:puts).with(13.0)
+
+      expect(STDIN).to receive(:gets).and_return('13 -')
+      expect(STDOUT).to receive(:puts).with(0.0)
+
+      expect(STDIN).to receive(:gets).and_return('q')
+      subject.run
+    end
+
+    it 'processes test case #3 and quits' do
+      expect(STDIN).to receive(:gets).and_return('-3')
+      expect(STDOUT).to receive(:puts).with(-3.0)
+
+      expect(STDIN).to receive(:gets).and_return('-2')
+      expect(STDOUT).to receive(:puts).with(-2.0)
+
+      expect(STDIN).to receive(:gets).and_return('*')
+      expect(STDOUT).to receive(:puts).with(6.0)
+
+      expect(STDIN).to receive(:gets).and_return('5')
+      expect(STDOUT).to receive(:puts).with(5.0)
+
+      expect(STDIN).to receive(:gets).and_return('+')
+      expect(STDOUT).to receive(:puts).with(11.0)
+
+      expect(STDIN).to receive(:gets).and_return('q')
+      subject.run
+    end
+
+    it 'processes test case #4 and quits' do
+      expect(STDIN).to receive(:gets).and_return('5')
+      expect(STDOUT).to receive(:puts).with(5.0)
+
+      expect(STDIN).to receive(:gets).and_return('9')
+      expect(STDOUT).to receive(:puts).with(9.0)
+
+      expect(STDIN).to receive(:gets).and_return('1')
+      expect(STDOUT).to receive(:puts).with(1.0)
+
+      expect(STDIN).to receive(:gets).and_return('-')
+      expect(STDOUT).to receive(:puts).with(8.0)
+
+      expect(STDIN).to receive(:gets).and_return('/')
+      expect(STDOUT).to receive(:puts).with(0.625)
 
       expect(STDIN).to receive(:gets).and_return('q')
       subject.run
